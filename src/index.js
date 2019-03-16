29cb48f6f988c9c104ae22dfe59b9085181c1a7d
Krzysztof Nowicki <krzysztof11nowicki@gmail.com>
2019-03-16 15:43:19 +0100

    main screen footer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import "./main.scss"; // important for build - do not remove
 // import { importTest } from "./js/someJSModule"; // import example
 
+const menuIcon = document.getElementById("menu");
+const footer = document.getElementById("footer") 
+const body = document.getElementById("body");
 
+menuIcon.addEventListener("click", function(){
 
-
+    if (footer.style.display === "flex") {
+        footer.style.display = "none";       
+    } else {
+        footer.style.display = "flex";        
+    }       
+    window.scrollTo(0, 1000);
+    });
