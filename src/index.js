277ec1d4ba9c4316aad0032f0566a0c07205cb7b
Krzysztof Nowicki <krzysztof11nowicki@gmail.com>
2019-03-17 15:34:42 +0100

    footer animation

--- src/index.js
+++ src/index.js
@@ -2,15 +2,28 @@ import "./main.scss"; // important for build - do not remove
 // import { importTest } from "./js/someJSModule"; // import example
 
 const menuIcon = document.getElementById("menu");
-const footer = document.getElementById("footer") 
-const body = document.getElementById("body");
+const footer = document.getElementById("footer");
+// const body = document.getElementById("body");
 
 menuIcon.addEventListener("click", function(){
+    let cl = footer.classList;    
+    if (!cl.contains("fade-in-bottom") && !cl.contains("fade-out-bottom")) {
+        footer.style.display = "flex";
+        cl.add("fade-in-bottom");
 
-    if (footer.style.display === "flex") {
-        footer.style.display = "none";       
-    } else {
-        footer.style.display = "flex";        
-    }       
-    window.scrollTo(0, 1000);
-    });
+    } else if(cl.contains("fade-in-bottom")){
+        cl.remove("fade-in-bottom");
+        cl.add("fade-out-bottom");
+        setTimeout(function(){
+            footer.style.display = "none";
+        },600)
+        
+
+    } else if(cl.contains("fade-out-bottom")){
+        cl.remove("fade-out-bottom");
+        cl.add("fade-in-bottom");
+        footer.style.display = "flex";
+    }
+});
+
+// 
