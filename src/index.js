d3d2985891bd4630a7a6018851c534a61be3845e
Krzysztof Nowicki <krzysztof11nowicki@gmail.com>
2019-04-28 15:54:36 +0200

    tester side html+ experience clock js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import "./main.scss"; // important for build - do not remove
 import { tooltip } from "./js/tooltip";
-// import { experience_clock } from "./js/experience_clock";
-import { sideNavHandler } from "./js/sideNavHandler";
-
+import { experience_clock } from "./js/experience_clock";
 
 document.addEventListener("DOMContentLoaded", function() {
     tooltip();
-    // sideNavHandler()
-    
-    
-    
+    experience_clock("Sep 1, 2017 00:00:01", "experience-clock");
   });
 
+  
--- src/js/experience_clock.js
+++ src/js/experience_clock.js
@@ -1,16 +1,22 @@
-const startDate = new Date("Sep 1, 2017 00:00:01").getTime();
-
-export function experience_clock() {
-    setInterval(function () {
-
-        let data = new Date();
-        let distance = data - startDate;
-        
-        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    
-            document.getElementById("counter").innerHTML = days + " dni " + hours + " godzin " +
-            minutes + " minut";
-    }, 1000);
-};
+  export function experience_clock(countFrom, id) {
+            setInterval(function () {
+                let startDate = new Date(countFrom).getTime();
+                let now = new Date();
+                let timeDifference = (now - startDate);
+                
+                let secondsInADay = 60 * 60 * 1000 * 24;
+                let secondsInAHour = 60 * 60 * 1000;
+                
+                let days = Math.floor(timeDifference / (secondsInADay) * 1);
+                let years = Math.floor(days / 365);
+                    if (years >= 1){ 
+                        days = days - (years * 365) 
+                    }
+                let hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
+            
+                let idEl = document.getElementById(id);
+                idEl.getElementsByClassName("years")[0].innerHTML = years;
+                idEl.getElementsByClassName("days")[0].innerHTML = days;
+                idEl.getElementsByClassName("hours")[0].innerHTML = hours;
+        }, 1000);
+  }
